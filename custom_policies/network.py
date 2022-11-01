ac1b7f10b16df14a6cfee63a6e5e0752a5d828b3
MaxSloof-Deloitte <115638487+MaxSloof-Deloitte@users.noreply.github.com>
2022-11-01 15:28:51 +0100

    Update network.py

--- custom_policies/network.py
+++ custom_policies/network.py
@@ -3,26 +3,12 @@ from checkov.cloudformation.checks.resource.aws.AbsSecurityGroupUnrestrictedIngr
 )
 
 
-class SecurityGroupUnrestrictedIngress1234(AbsSecurityGroupUnrestrictedIngress):
-    def __init__(self):
-        super().__init__(check_id="CKV_AWS_MAX1", port=1234)
+class SecurityGroupUnrestrictedIngressAny(AbsSecurityGroupUnrestrictedIngress):
+    def __init__(self, check_id, test_port):
+        super().__init__(check_id=check_id, port=test_port)
 
 
-check = SecurityGroupUnrestrictedIngress1234()
-
-
-class SecurityGroupUnrestrictedIngress443(AbsSecurityGroupUnrestrictedIngress):
-    def __init__(self):
-        super().__init__(check_id="CKV_AWS_MAX2", port=443)
-
-
-check = SecurityGroupUnrestrictedIngress443()
-
-
-class SecurityGroupUnrestrictedIngress2222(AbsSecurityGroupUnrestrictedIngress):
-    def __init__(self):
-        for ip in [2222, 25]:
-            super().__init__(check_id=f"CKV_AWS_MAX{ip}", port=ip)
-
-
-check = SecurityGroupUnrestrictedIngress2222()
+for port in [25, 443, 2222]:
+    SecurityGroupUnrestrictedIngressAny(
+        check_id=f"CKV_AWS_CUSTOM_PORT{port}", test_port=port
+    )
